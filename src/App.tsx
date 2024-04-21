ecd0c21903425b31f4f1ef0df8f56f15d2818031
tri.luu@arenacommerce.com <tri.luu@arenacommerce.com>
2024-04-21 23:51:27 +0700

    feat: handle routes with hook useRoutes, add page and layout folder, component register, login, productList added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import useRouteElement from './useRouteElement'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const routeElements = useRouteElement()
 
-  return (
-    <>
-      <div className='bg-red-500'>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
-  )
+  return <div>{routeElements}</div>
 }
 
 export default App
